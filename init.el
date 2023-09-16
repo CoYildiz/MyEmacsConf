a2bd26728655973f99292b33d75feca5c7782403
CoYildiz <yusufyildizr@gmail.com>
2023-09-16 12:35:04 +0300

    Init.el ve myinit.org 4.bolum eklendi

---
+++ init.el
@@ -0,0 +1,21 @@
+(org-babel-load-file
+ (expand-file-name
+  "myinit.org"
+  user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default))
+ '(package-selected-packages
+   '(magit yasnippet-classic-snippets general hydra helpful ivy-rich rainbow-delimiters ace-window tabbar doom-theme which-key doom-modeline doom-themes timu-rouge-theme dracula-theme timu-spacegrey-theme use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 2.0)))))
+
